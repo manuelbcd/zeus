bdf0f2341575b43dfa778cce1763184221683c21
Marco Davalos <marcodavalosmantilla@gmail.com>
2017-12-18 21:28:33 +0100

    go.dockerfile to go.build

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # zeus
-It is a simple example of a Go web application authenticating with GitHub as a GitHub OAuth App using OAuth 2.0 industry-standard protocol.
-
-## Authentication Flow
-![authentication-flow](/oauth2-flow.png "Application Authentication flow")
+### 404 Still thinking about how to explain it, it will be soon.
--- main.go
+++ main.go
@@ -1,24 +1,29 @@
 package utils
 
 import (
-    "os"
     "log"
+    "flag"
     "net/http"
     "github.com/gorilla/mux"
     "github.com/marco2704/zeus/oauth2"
     "github.com/marco2704/zeus/users"
 )
 
-var (
-    serverListeningURL = os.Getenv("LISTENING_ADDRESS")
+const (
+    addrName = "addr"
+    addrUsage = "address where the go server is going to be listening"
+    addrDefaultValue = ":80"
 )
 
 func main() {
+
+    addr := flag.String(addrName, addrDefaultValue, addrUsage)
+
     router := mux.NewRouter()
     router.HandleFunc("/", oauth2.GitHubSignIn).Methods("GET")
     router.HandleFunc("/home", oauth2.GitHubCallBack).Methods("GET")
     router.HandleFunc("/oauth2/github", oauth2.GitHubOAuth).Methods("GET")
     router.HandleFunc("/users", users.CreateUser).Methods("POST")
     router.HandleFunc("/users/{id}", users.GetUser).Methods("GET")
-    log.Fatal(http.ListenAndServe(serverListeningURL, router))
+    log.Fatal(http.ListenAndServe(*addr, router))
 }
